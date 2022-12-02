a1f56b323760eff91deab6fc281bc5d522c72cf5
Janos Nanai <janos.nanai@gmail.com>
2022-12-02 20:48:12 +0100

    svelte added

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import svelte from "@astrojs/svelte";
+
+// https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  output: "server",
+  integrations: [tailwind(), svelte()],
+  output: "server"
 });
