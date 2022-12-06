800d765244d6daeb06a899af9abd361fc833e8ca
Janos Nanai <janos.nanai@gmail.com>
2022-12-07 00:56:54 +0100

    spicy svelte navbar

---
+++ src/components/navbar.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  export let topfixed = false;
+  const navItems = [
+    { href: "#about", title: "about" },
+    { href: "#projects", title: "projects" },
+    { href: "#contact", title: "contact" },
+  ];
+</script>
+
+<header
+  id="header"
+  class="absolute z-10 bg-transparent w-full bottom-0"
+  class:topfixed
+>
+  <div class="flex justify-between items-center container mx-auto mt-1 mb-3">
+    <a href="#hero" class="text-zinc-50 text-2xl p-2">janosnanai.dev</a>
+    <nav>
+      {#each navItems as { href, title }}
+        <a
+          {href}
+          class="text-zinc-400 p-2 text-xl bg-transparent hover:bg-white/5 hover:text-zinc-100"
+        >
+          {title}
+        </a>
+      {/each}
+    </nav>
+  </div>
+</header>
+
+<style>
+  .topfixed {
+    position: fixed;
+    top: 0;
+  }
+</style>
---
+++ src/components/sections/hero.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import { onMount, onDestroy } from "svelte";
+  import Navbar from "../navbar.svelte";
+
+  let topfixed = false;
+  let navGuardPosY: string;
+
+  const intersectionObserver = new IntersectionObserver((entries) => {
+    if (typeof window === "undefined") return;
+
+    if (!entries[0].isIntersecting) {
+      topfixed = true;
+    } else {
+      topfixed = false;
+    }
+  });
+
+  onMount(() => {
+    const navGuardEl = document.getElementById("nav-guard") as HTMLElement;
+    const headerEl = document.getElementById("header") as HTMLElement;
+
+    navGuardPosY = (headerEl.getBoundingClientRect().height ?? 0) + "px";
+
+    intersectionObserver.observe(navGuardEl);
+  });
+
+  onDestroy(() => {
+    intersectionObserver.disconnect();
+  });
+</script>
+
+<section
+  id="hero"
+  class="h-screen bg-zinc-900 flex flex-col items-center justify-center relative"
+>
+  <h1 class="text-zinc-50 text-6xl mb-7">
+    <span class="block text-zinc-400 text-3xl mb-3">the personal site of</span>
+    <span class="block">János Nánai</span>
+  </h1>
+  <h4 class="text-zinc-500">engineer - web developer</h4>
+  <Navbar {topfixed} />
+  <div
+    id="nav-guard"
+    class="-z-50 bg-transparent absolute bottom-0 h-0"
+    style:bottom={navGuardPosY}
+  />
+</section>
