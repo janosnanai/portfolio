9e59827b93390e20039348eb87c997ab1d391a17
Janos Nanai <janos.nanai@gmail.com>
2022-12-06 15:59:23 +0100

    form style

--- src/components/contact-form.svelte
+++ src/components/contact-form.svelte
@@ -27,14 +27,26 @@
 
 <form on:submit|preventDefault={handleSubmit} class="p-7">
   <div class="flex flex-col gap-3">
-    <input bind:value={name} id="name" type="text" placeholder="your name" />
+    <input
+      bind:value={name}
+      id="name"
+      type="text"
+      placeholder="your name"
+      class="bg-transparent border-b p-2"
+    />
     <input
       bind:value={emailAddress}
       id="email-address"
       type="text"
       placeholder="your email"
+      class="bg-transparent border-b p-2"
     />
-    <textarea bind:value={message} id="message" placeholder="your message" />
+    <textarea
+      bind:value={message}
+      id="message"
+      placeholder="your message"
+      class="p-2 bg-black/5 border-b min-h-[3rem] max-h-[9rem]"
+    />
     <button class="p-2 bg-fuchsia-500" type="submit">send test email</button>
   </div>
 </form>
