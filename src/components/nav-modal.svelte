046ad524cf78db23ac3ea9509d2e84d6207175b1
Janos Nanai <janos.nanai@gmail.com>
2022-12-08 16:00:31 +0100

    feat: modal trap focus and esc

--- src/components/nav-modal.svelte
+++ src/components/nav-modal.svelte
@@ -1,20 +1,69 @@
 <script lang="ts">
   import { createEventDispatcher, onMount, onDestroy } from "svelte";
 
-  import { NAV_ITEMS } from "../utils/consts";
+  import { NAV_ITEMS, FOCUSABLE_ELEMENTS } from "../utils/consts";
 
   const dispatch = createEventDispatcher();
+
+  let originalOverflow: string;
+  let focusableNodes: HTMLElement[];
+  let self: HTMLElement;
 
   function handleClose() {
     dispatch("close");
   }
 
-  let originalOverflow: string;
+  function focusFirst() {
+    if (focusableNodes.length === 0) return;
+
+    focusableNodes[0].focus();
+  }
+
+  function focusAround(e: KeyboardEvent) {
+    // trap focus
+    if (focusableNodes.length === 0) return;
+
+    if (!self.contains(document.activeElement)) {
+      focusFirst();
+      return;
+    }
+
+    const idx = focusableNodes.indexOf(document.activeElement as HTMLElement);
+
+    if (e.shiftKey && idx === 0) {
+      e.preventDefault();
+      focusableNodes[focusableNodes.length - 1].focus();
+      return;
+    }
+
+    if (!e.shiftKey && idx === focusableNodes.length - 1) {
+      e.preventDefault();
+      focusableNodes[0].focus();
+      return;
+    }
+  }
+
+  function handleKeydown(e: KeyboardEvent) {
+    switch (e.key) {
+      case "Escape":
+        handleClose();
+        break;
+      case "Tab":
+        focusAround(e);
+        break;
+    }
+  }
 
   onMount(() => {
     // disable scrolling, also remember prev state
     originalOverflow = document.body.style.overflow;
     document.body.style.overflow = "hidden";
+
+    focusableNodes = Array(
+      ...self.querySelectorAll(FOCUSABLE_ELEMENTS.join(","))
+    ) as HTMLElement[];
+
+    focusFirst();
   });
 
   onDestroy(() => {
@@ -23,7 +72,11 @@
   });
 </script>
 
-<div class="fixed z-10 inset-0 h-screen min-h-[20rem] bg-indigo-900/90">
+<div
+  class="fixed z-10 inset-0 h-screen min-h-[20rem] bg-indigo-900/90"
+  bind:this={self}
+  on:keydown={handleKeydown}
+>
   <button
     on:click={handleClose}
     class="fixed top-5 right-5 text-zinc-400 p-2 text-xl bg-transparent hover:bg-white/5 hover:text-zinc-100"
--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -3,3 +3,17 @@ export const NAV_ITEMS = [
   { href: "#projects", title: "projects" },
   { href: "#contact", title: "contact" },
 ];
+
+export const FOCUSABLE_ELEMENTS = [
+  "a[href]",
+  "area[href]",
+  'input:not([disabled]):not([type="hidden"]):not([aria-hidden])',
+  "select:not([disabled]):not([aria-hidden])",
+  "textarea:not([disabled]):not([aria-hidden])",
+  "button:not([disabled]):not([aria-hidden])",
+  "iframe",
+  "object",
+  "embed",
+  "[contenteditable]",
+  '[tabindex]:not([tabindex^="-"])',
+];
