6896fc627da022c213e308244bd9975ca5b26fda
Janos Nanai <janos.nanai@gmail.com>
2022-12-09 13:36:21 +0100

    spicy astro-ish hero-card

--- src/components/sections/hero.svelte
+++ src/components/sections/hero.svelte
@@ -2,13 +2,8 @@
   import { onMount, onDestroy } from "svelte";
   import Navbar from "../navbar.svelte";
 
-  interface I_MousePos {
-    x: number | null;
-    y: number | null;
-  }
-
   let topfixed = false;
-  let mousePos: I_MousePos = { x: null, y: null };
+  let mousePos = { x: null as number | null, y: null as number | null };
   let w: number;
   let h: number;
   let navGuardPosY: string;
@@ -18,7 +13,6 @@
 
   function handleMouseLeave() {
     mousePos = { x: null, y: null };
-    console.log("mouse left");
   }
 
   function handleMouseMove(e: MouseEvent) {
@@ -51,23 +45,34 @@
 
 <section
   id="hero"
-  class="h-screen min-h-[20rem] bg-zinc-900 flex flex-col items-center justify-center relative"
+  class="h-screen min-h-[20rem] bg-zinc-900 flex flex-col items-center justify-center relative overflow-hidden"
 >
   <div
-    class="hero-card p-9"
     bind:clientWidth={w}
     bind:clientHeight={h}
     on:mouseleave={handleMouseLeave}
     on:mousemove={handleMouseMove}
+    class="hero-card"
     style:--rot-x={rotX}
     style:--rot-y={rotY}
   >
-    <h1 class="text-zinc-50 text-6xl mb-7">
-      <span class="block text-zinc-400 text-3xl mb-3">the personal site of</span
+    <div>
+      <h1 class="text-zinc-50 text-6xl mb-5">
+        <span class="block text-zinc-400 text-3xl mb-3"
+          >the personal site of</span
+        >
+        <span class="block">János Nánai</span>
+      </h1>
+      <span class="block text-yellow-500 uppercase text-sm"
+        >engineer, full-stack developer</span
       >
-      <span class="block">János Nánai</span>
-    </h1>
-    <span class="block text-zinc-500">engineer, full-stack developer</span>
+    </div>
+    {#each [-60, -40, -20, 0, 20, 40, 60] as x ("gridX_" + x)}
+      <div class="grid-line vertical" style={`right: ${x + 40}%;`} />
+    {/each}
+    {#each [-20, 0, 20, 40] as y ("gridY_" + y)}
+      <div class="grid-line horizontal" style={`top: ${(y * w) / h + 40}%;`} />
+    {/each}
   </div>
   <Navbar {topfixed} />
   <div
@@ -80,6 +85,19 @@
 <style>
   .hero-card {
     transform: perspective(800px) rotateX(var(--rot-x)) rotateY(var(--rot-y));
-    transition-duration: 200ms;
+    transform-style: preserve-3d;
+    @apply p-20 duration-[50] relative mr-52 mt-16;
+  }
+  .grid-line {
+    @apply absolute from-transparent via-violet-500 to-transparent -z-10;
+    @apply -translate-y-2/3;
+    @apply -translate-x-1/4;
+  }
+  .grid-line.vertical {
+    @apply w-[1px] h-[180%] bg-gradient-to-t;
+  }
+
+  .grid-line.horizontal {
+    @apply w-[200%] h-[1px] bg-gradient-to-r;
   }
 </style>
