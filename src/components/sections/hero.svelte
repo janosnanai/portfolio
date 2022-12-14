c55bb8e218a2d07d65d32db469b11466ae92c5c1
Janos Nanai <janos.nanai@gmail.com>
2022-12-15 00:31:55 +0100

    fix floaty space business card

--- src/components/sections/hero.svelte
+++ src/components/sections/hero.svelte
@@ -3,21 +3,37 @@
   import Navbar from "../navbar.svelte";
   import Starfield from "../starfield.svelte";
 
+  const maxAngleX = 15;
+  const maxAngleY = 15;
+
   let topfixed = false;
   let mousePos = { x: null as number | null, y: null as number | null };
-  let w: number;
-  let h: number;
+  let headingWidth: number;
+  let headingHeight: number;
+  let sectionWidth: number;
+  let sectionHeight: number;
   let navGuardPosY: string;
 
-  $: rotY = (!!mousePos.x ? (mousePos.x - w / 2) / (w / 7) : 0) + "deg";
-  $: rotX = (!!mousePos.y ? -(mousePos.y - h / 2) / (h / 7) : 0) + "deg";
+  $: rot_x =
+    (!!mousePos.y ? maxAngleX * (mousePos.y / sectionHeight - 0.5) : 0) + "deg";
+  $: rot_y =
+    (!!mousePos.x ? -(maxAngleY * (mousePos.x / sectionWidth - 0.5)) : 0) +
+    "deg";
+  $: offset_x =
+    (!!mousePos.x
+      ? (headingWidth / 8) * (mousePos.x / sectionWidth - 0.5)
+      : 0) + "px";
+  $: offset_y =
+    (!!mousePos.y
+      ? (headingHeight / 8) * (mousePos.y / sectionHeight - 0.5)
+      : 0) + "px";
 
   function handleMouseLeave() {
     mousePos = { x: null, y: null };
   }
 
   function handleMouseMove(e: MouseEvent) {
-    mousePos = { x: e.offsetX, y: e.offsetY };
+    mousePos = { x: e.clientX, y: e.clientY };
   }
 
   const intersectionObserver = new IntersectionObserver((entries) => {
@@ -45,37 +61,46 @@
 </script>
 
 <section
+  bind:clientHeight={sectionHeight}
+  bind:clientWidth={sectionWidth}
+  on:mouseleave={handleMouseLeave}
+  on:mousemove={handleMouseMove}
   id="hero"
-  class="h-screen min-h-[20rem] bg-black flex flex-col items-center justify-center relative overflow-hidden"
+  class="h-screen min-h-[20rem] bg-black relative overflow-hidden"
 >
   <Starfield />
-  <div
-    bind:clientWidth={w}
-    bind:clientHeight={h}
-    on:mouseleave={handleMouseLeave}
-    on:mousemove={handleMouseMove}
-    class="hero-card"
-    style:--rot-x={rotX}
-    style:--rot-y={rotY}
-  >
-    <div>
-      <h1 class="text-7xl mb-5">
-        <span class="block text-sirocco-300/50 text-2xl mb-2.5"
-          >the personal site of</span
+  <div class="absolute bottom-1/4 left-1/4 -ml-24">
+    <div
+      class="hero-card relative"
+      bind:clientWidth={headingWidth}
+      bind:clientHeight={headingHeight}
+      style:--rot-x={rot_x}
+      style:--rot-y={rot_y}
+      style:--offset-y={offset_y}
+      style:--offset-x={offset_x}
+    >
+      <div class="hero-card--front z-10">
+        <h1 class="text-7xl mb-5">
+          <span class="block text-sirocco-300/50 text-2xl mb-2.5"
+            >the personal site of</span
+          >
+          <span class="block text-sirocco-50">János Nánai</span>
+        </h1>
+        <!-- <div class="w-1/3 h-1 bg-gigas-500 mb-3.5" /> -->
+        <span class="block text-trinidad-400 uppercase text-sm"
+          >engineer, full-stack developer</span
         >
-        <span class="block text-sirocco-50">János Nánai</span>
-      </h1>
-      <div class="w-1/3 h-1 bg-gigas-500 mb-3.5" />
-      <span class="block text-sirocco-100/50 uppercase text-sm"
-        >engineer, full-stack developer</span
-      >
+      </div>
+      {#each [-35, -20, -5, 10, 25, 40, 55] as x ("gridX_" + x)}
+        <div class="grid-line vertical" style={`right: ${x + 40}%;`} />
+      {/each}
+      {#each [-15, 0, 15, 30] as y ("gridY_" + y)}
+        <div
+          class="grid-line horizontal"
+          style={`top: ${(y * headingWidth) / headingHeight + 40}%;`}
+        />
+      {/each}
     </div>
-    <!-- {#each [-50, -30, -10, 10, 30, 50] as x ("gridX_" + x)}
-      <div class="grid-line vertical" style={`right: ${x + 40}%;`} />
-    {/each}
-    {#each [-25, -5, 15, 35] as y ("gridY_" + y)}
-      <div class="grid-line horizontal" style={`top: ${(y * w) / h + 40}%;`} />
-    {/each} -->
   </div>
   <Navbar {topfixed} />
   <div
@@ -87,14 +112,20 @@
 
 <style>
   .hero-card {
-    transform: perspective(800px) rotateX(var(--rot-x)) rotateY(var(--rot-y));
-    transform-style: preserve-3d;
     @apply p-20 relative mr-52 mt-16;
+    transform: perspective(800px) rotateX(var(--rot-x)) rotateY(var(--rot-y))
+      translate(var(--offset-x), var(--offset-y));
+    transform-style: preserve-3d;
+    transition: transform 0.6s cubic-bezier(0.23, 1, 0.32, 1);
   }
-  /* .grid-line {
-    @apply absolute from-transparent via-violet-500 to-transparent -z-10;
-    @apply -translate-y-2/3;
-    @apply -translate-x-1/4;
+
+  .hero-card--front {
+    transform: translateZ(40px);
+  }
+
+  .grid-line {
+    @apply absolute from-transparent via-gigas-400/90 to-transparent -z-10;
+    @apply -translate-x-1/4 -translate-y-2/3;
   }
   .grid-line.vertical {
     @apply w-[1px] h-[180%] bg-gradient-to-t;
@@ -102,5 +133,5 @@
 
   .grid-line.horizontal {
     @apply w-[180%] h-[1px] bg-gradient-to-r;
-  } */
+  }
 </style>
